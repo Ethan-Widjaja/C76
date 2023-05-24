e32cc28335be6dcb1064bfc95a00bf011e1c53d1
Ethan-Widjaja <122017519+Ethan-Widjaja@users.noreply.github.com>
2023-05-24 20:20:34 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+console.log("hello");
+
+var f_name="ethan";
+console.log(f_name);
+
+//concat -to join
+
+var a=f_name.concat(f_name);
+console.log(a);
+
+//length
+
+var b=a.length
+console.log(b);
+
+//replace
+
+var c="i am coding student"
+console.log(c);
+
+var d=c.replace("student","developer");
+console.log(d);
+
+//lower case
+
+var d="EthAn"
+var c=d.toLowerCase()
+console.log(c);
+
+//upper case
+
+var q="EthAn"
+var z=q.toUpperCase()
+console.log(z);
+
+//max
+
+var i=Math.max(50,1,5,100)
+console.log(i);
+
+//min
+
+var i=Math.min(50,1,5,100);
+console.log(i);
+
+//Square root
+
+var k=Math.sqrt(16);
+console.log(k);
+
+//add/subtract/multiply,etc...
+
+var p=4;
+var s=3;
+var r=p+s;
+console.log(r);
+
+var n_no=10;
+var m_no=2;
+console.log(n_no+m_no);
